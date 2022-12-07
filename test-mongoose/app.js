ff96b2cfc176deb79def95fc8088fe80b6fef219
khw7876 <khw7876@gmail.com>
2022-12-07 13:47:31 +0900

    ADD test-mongoose

--- test-mongoose/app.js
+++ test-mongoose/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,28 +13,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(cookieParser('secret code'));
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: "secret code",
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  }
-}));
-
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function(req, res, next){
-  console.log("여기경로 : ", path.join(__dirname, 'public'));
-  next();
-});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
